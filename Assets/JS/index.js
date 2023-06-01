093e78724ea728f0fc516cdd33fd54482f3943ab
davidmicah123 <micahdanielsd@gmail.com>
2023-06-01 21:17:36 +0100

    Adding mobile navbar

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,1 +1,8 @@
-// const mobileNavToogler = document
+const oepnMobileNav = () => {
+    const mobileNav = document.querySelector('.mobile_nav');
+    mobileNav.classList.remove('hide');
+}
+const closeMobileNav = () => {
+    const mobileNav = document.querySelector('.mobile_nav');
+    mobileNav.classList.add('hide');
+}
