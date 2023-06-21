33acfd561ff2055d2fe9c594a6f84fcaa9c9e480
onknown54 <lekanm715@gmail.com>
2023-06-22 00:50:53 +0100

    added sticky navbar to project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
 const oepnMobileNav = () => {
-    const mobileNav = document.querySelector('.mobile_nav');
-    mobileNav.classList.remove('hide');
-}
+  const mobileNav = document.querySelector(".mobile_nav");
+  mobileNav.classList.remove("hide");
+};
+
 const closeMobileNav = () => {
-    const mobileNav = document.querySelector('.mobile_nav');
-    mobileNav.classList.add('hide');
-}
+  const mobileNav = document.querySelector(".mobile_nav");
+  mobileNav.classList.add("hide");
+};
+
+let navbar = document.querySelector(".hero_section .navbar"),
+  stick = navbar.getBoundingClientRect().top;
+console.log(stick);
+
+window.addEventListener("scroll", (e) => {
+  let scrollPos = window.scrollY;
+  if (scrollPos > stick) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
